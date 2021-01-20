057e6fbbf460cd5b524e28dedb2971b1a5ab5168
Daniel Schneider <daniel.schneider@eramux.com>
2021-01-20 21:43:11 +0100

    fix: only publish result if a result queue is set

--- shims/nats/shim.go
+++ shims/nats/shim.go
@@ -96,9 +96,11 @@ func (n *natsshim) Stop() {
 
 // Publish result will publish the worker result to the message queue
 func (n *natsshim) PublishResult(config config.Config, msg []byte) {
-	err := n.stanConnection.Publish(config.BrokerResultSubject, msg)
-	if err != nil {
-		log.Fatal("Could not Publish result: ", string(msg))
+	if config.BrokerResultSubject != "" {
+		err := n.stanConnection.Publish(config.BrokerResultSubject, msg)
+		if err != nil {
+			log.Fatal("Could not Publish result: ", string(msg))
+		}
 	}
 }
 
