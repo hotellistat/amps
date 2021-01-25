ddf8d94ee769f3f1b42dcfc3bb7b55dc7d9eedeb
Daniel Schneider <daniel.schneider@eramux.com>
2021-01-25 22:39:09 +0100

    refactor:  extract functions

--- cmd/main.go
+++ cmd/main.go
@@ -37,6 +37,63 @@ type BrokerShim interface {
 	PublishResult(config.Config, event.Event) error
 }
 
+func main() {
+	godotenv.Load()
+
+	conf := config.New()
+
+	brokerTypes := map[string]BrokerShim{
+		"nats":  &natsshim.NatsBroker{},
+		"kafka": &kafkashim.KafkaBroker{},
+	}
+
+	broker, ok := brokerTypes[conf.BrokerType]
+
+	if !ok {
+		log.Fatal("Could not find broker type", conf.BrokerType)
+	}
+
+	var mutex = &sync.Mutex{}
+
+	// Initialize our job manifest. This will hold all currently active jobs for this worker
+	jobManifest := make(map[string]Job, conf.MaxConcurrency)
+
+	// Initialize a new broker instance, which is a general abstraction of the NATS go library
+	broker.Initialize(*conf)
+
+	// Create a new subscription for nats streaming
+	broker.Start(func(msg *stan.Msg) {
+		messageHandler(msg, conf, jobManifest, &broker, mutex)
+	})
+
+	// This endpoint is the checkout endpoint, where workloads can notify nats, that they have finished
+	http.HandleFunc("/checkout", func(w http.ResponseWriter, req *http.Request) {
+		jobCheckout(w, req, conf, jobManifest, &broker, mutex)
+	})
+
+	// The watchdog, if enabled, checks the timeout of each Job and deletes it if it got too old
+	if conf.JobTimeout != 0 {
+		go watchdog(conf, jobManifest, &broker, mutex)
+	}
+
+	go http.ListenAndServe(":4000", nil)
+
+	// General signal handling to teardown the worker
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		for range signalChan {
+			fmt.Printf("\nReceived an interrupt, closing connection...\n\n")
+			broker.Teardown()
+
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
+
+}
+
 func insertJob(
 	event cloudevents.Event,
 	jobManifest map[string]Job,
@@ -112,7 +169,7 @@ func messageHandler(
 	msg *stan.Msg,
 	conf *config.Config,
 	jobManifest map[string]Job,
-	broker BrokerShim,
+	broker *BrokerShim,
 	mutex *sync.Mutex) {
 
 	event := cloudevents.NewEvent()
@@ -137,7 +194,7 @@ func messageHandler(
 	go triggerWorkload(event, *conf, &waitgroup, workloadErr)
 
 	insertJobErr := make(chan error, 1)
-	go insertJob(event, jobManifest, &broker, *conf, mutex, &waitgroup, insertJobErr)
+	go insertJob(event, jobManifest, broker, *conf, mutex, &waitgroup, insertJobErr)
 
 	workloadErrResult := <-workloadErr
 	insertJobErrResult := <-insertJobErr
@@ -155,140 +212,102 @@ func messageHandler(
 		msg.Ack()
 	} else {
 		log.Println("Job ID:", event.Context.GetID(), "could not be acknowledged")
+		print(insertJobErrResult.Error())
 	}
 }
 
-func main() {
-	godotenv.Load()
+func jobCheckout(
+	w http.ResponseWriter,
+	req *http.Request,
+	conf *config.Config,
+	jobManifest map[string]Job,
+	broker *BrokerShim,
+	mutex *sync.Mutex) {
 
-	conf := config.New()
+	cloudevent := cloudevents.NewEvent()
 
-	brokerTypes := map[string]BrokerShim{
-		"nats":  &natsshim.NatsBroker{},
-		"kafka": &kafkashim.KafkaBroker{},
+	body, readErr := ioutil.ReadAll(req.Body)
+
+	if readErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Could not read request Body"))
+		return
 	}
 
-	broker, ok := brokerTypes[conf.BrokerType]
+	ceErr := json.Unmarshal(body, &cloudevent)
 
-	if !ok {
-		log.Fatal("Could not find broker type", conf.BrokerType)
+	if ceErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Can not unmarshal cloudevent, make sure you send a cloudevent in structured content mode"))
+		return
 	}
 
-	// Initialize our job manifest. This will hold all currently active jobs for this worker
-	jobManifest := make(map[string]Job, conf.MaxConcurrency)
+	// Fetch the nopublish event context extension. This will prevent publishing the recieved event to our broker.
+	// This is normally used, if you want to define the end of a chain of workloads, where the last link of the chain
+	// Should not create any new events in the broker anymore
+	data, _ := cloudevent.Context.GetExtension("nopublish")
 
-	// Initialize a new broker instance, which is a general abstraction of the NATS go library
-	broker.Initialize(*conf)
+	jobID := cloudevent.Context.GetID()
 
-	var mutex = &sync.Mutex{}
+	if conf.Debug {
+		log.Println("Deleting Job ID:", jobID)
+	}
 
-	// Create a new subscription for nats streaming
-	broker.Start(func(msg *stan.Msg) {
-		messageHandler(msg, conf, jobManifest, broker, mutex)
-	})
-
-	// This endpoint is the checkout endpoint, where workloads can notify nats, that they have finished
-	http.HandleFunc("/checkout", func(w http.ResponseWriter, req *http.Request) {
-
-		cloudevent := cloudevents.NewEvent()
-
-		body, readErr := ioutil.ReadAll(req.Body)
-
-		if readErr != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Could not read request Body"))
-			return
-		}
-
-		ceErr := json.Unmarshal(body, &cloudevent)
-
-		if ceErr != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Can not unmarshal cloudevent, make sure you send a cloudevent in structured content mode"))
-			return
-		}
-
-		// Fetch the nopublish event context extension. This will prevent publishing the recieved event to our broker.
-		// This is normally used, if you want to define the end of a chain of workloads, where the last link of the chain
-		// Should not create any new events in the broker anymore
-		data, _ := cloudevent.Context.GetExtension("nopublish")
-
-		jobID := cloudevent.Context.GetID()
-
+	if data != true {
 		if conf.Debug {
-			log.Println("Deleting Job ID:", jobID)
+			log.Println("Publishing recieved event to broker")
 		}
-
-		if data != true {
-			if conf.Debug {
-				log.Println("Publishing recieved event to broker")
-			}
-			publishErr := broker.PublishResult(*conf, cloudevent)
-			if publishErr != nil {
-				log.Println("Could not publish event to broker")
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("Could not publish your event to the broker"))
-				// mutex.Unlock()
-				return
-			}
+		publishErr := (*broker).PublishResult(*conf, cloudevent)
+		if publishErr != nil {
+			log.Println("Could not publish event to broker")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Could not publish your event to the broker"))
+			// mutex.Unlock()
+			return
 		}
-
-		mutex.Lock()
-
-		delete(jobManifest, jobID)
-		if int(len(jobManifest)) < conf.MaxConcurrency {
-			// Initialize a new subscription should the old one have been closed
-			broker.Start(func(msg *stan.Msg) {
-				messageHandler(msg, conf, jobManifest, broker, mutex)
-			})
-		}
-		mutex.Unlock()
-
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte("OK"))
-	})
-
-	go http.ListenAndServe(":4000", nil)
-
-	// The watchdog, if enabled, checks the timeout of each Job and deletes it if it got too old
-	if conf.JobTimeout != 0 {
-		go func() {
-			for {
-				maxLifetime := conf.JobTimeout
-
-				for id, job := range jobManifest {
-					if time.Now().Sub(job.created) > maxLifetime {
-						if conf.Debug {
-							log.Println("Job ID:", id, "timed out")
-						}
-						delete(jobManifest, id)
-					}
-				}
-
-				if int(len(jobManifest)) < conf.MaxConcurrency {
-					broker.Start(func(msg *stan.Msg) {
-						messageHandler(msg, conf, jobManifest, broker, mutex)
-					})
-				}
-
-				sleepTime, _ := time.ParseDuration("250ms")
-				time.Sleep(sleepTime)
-			}
-		}()
 	}
 
-	// General signal handling to teardown the worker
-	signalChan := make(chan os.Signal, 1)
-	cleanupDone := make(chan bool)
-	signal.Notify(signalChan, os.Interrupt)
-	go func() {
-		for range signalChan {
-			fmt.Printf("\nReceived an interrupt, closing connection...\n\n")
-			broker.Teardown()
+	mutex.Lock()
 
-			cleanupDone <- true
+	delete(jobManifest, jobID)
+	if int(len(jobManifest)) < conf.MaxConcurrency {
+		// Initialize a new subscription should the old one have been closed
+		(*broker).Start(func(msg *stan.Msg) {
+			messageHandler(msg, conf, jobManifest, broker, mutex)
+		})
+	}
+	mutex.Unlock()
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("OK"))
+}
+
+func watchdog(
+	conf *config.Config,
+	jobManifest map[string]Job,
+	broker *BrokerShim,
+	mutex *sync.Mutex) {
+	go func() {
+		for {
+			maxLifetime := conf.JobTimeout
+
+			for id, job := range jobManifest {
+				if time.Now().Sub(job.created) > maxLifetime {
+					if conf.Debug {
+						log.Println("Job ID:", id, "timed out")
+					}
+					delete(jobManifest, id)
+				}
+			}
+
+			if int(len(jobManifest)) < conf.MaxConcurrency {
+				(*broker).Start(func(msg *stan.Msg) {
+					messageHandler(msg, conf, jobManifest, broker, mutex)
+				})
+			}
+
+			sleepTime, _ := time.ParseDuration("250ms")
+			time.Sleep(sleepTime)
 		}
 	}()
-	<-cleanupDone
-
 }
