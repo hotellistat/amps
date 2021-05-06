ec63b12ddb51df5de0526a746d650eee86594fce
Daniel Schneider <daniel.schneider@eramux.com>
2021-05-06 09:43:34 +0200

    fix: natvie amqp max concurrency handling

--- hack/publish_prod.sh
+++ hack/publish_prod.sh
@@ -1,16 +1,16 @@
-go run ./pub.go -s nats.messaging:4222 -c \
+go run ./pub.go -s localhost:4222 -c \
 "nats" \
-"com.hotellistat.scraping" \
+"com.hotellistat.scraping.booking" \
 '{"specversion":"1.0",
-"type":"com.hotellistat.scraping",
+"type":"com.hotellistat.scraping.booking",
 "id":"'"$(uuidgen)"'",
 "source":"testing",
 "data":{
   "type":"auto",
   "identifier":"'"$(uuidgen)"'",
-  "ota_id":5,
+  "ota_id":1,
   "hotel_id":17,
-  "hotel_ota_id":"391295",
+  "hotel_ota_id":"de/rocco-forte-the-charles.de.html",
   "offset":0,
   "crawl_date":"2021-05-01",
   "days_to_crawl":2,
