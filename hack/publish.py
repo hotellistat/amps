236c9fc6c2f301f66f3377c4ca00b8697e49098c
Daniel Schneider <daniel.schneider@eramux.com>
2021-05-06 09:43:34 +0200

    fix: natvie amqp max concurrency handling

--- hack/publish.py
+++ hack/publish.py
@@ -1,36 +1,39 @@
 import pika
 import uuid
 import json
+import time
 
-message = {
-    "specversion": "1.0",
-    "type": "com.hotellistat.scraping",
-    "id": str(uuid.uuid4()),
-    "source": "testing",
-    "data": {
-        "type": "auto",
-        "identifier": str(uuid.uuid4()),
-        "ota_id": 5,
-        "hotel_id": 17,
-        "hotel_ota_id": "391295",
-        "offset": 0,
-        "crawl_date": "2021-05-01",
-        "days_to_crawl": 2,
-        "length_of_stay": 1,
-        "max_persons": 2,
-        "country_code": "de",
-        "currency": "EUR",
-        "closures": []
-    }
-}
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost', 5672))
-
 channel = connection.channel()
 
-channel.basic_publish(exchange='',
+
+for i in range(40):
+    message = {
+        "specversion": "1.0",
+        "type": "com.hotellistat.scraping",
+        "id": str(uuid.uuid4()),
+        "source": "testing",
+        "data": {
+            "type": "auto",
+            "identifier": str(uuid.uuid4()),
+            "ota_id": 1,
+            "hotel_id": 17,
+            "hotel_ota_id": "de/rocco-forte-the-charles.de.html",
+            "offset": 0,
+            "crawl_date": "2021-05-01",
+            "days_to_crawl": 1,
+            "length_of_stay": 1,
+            "max_persons": 2,
+            "country_code": "de",
+            "currency": "EUR",
+            "closures": []
+        }
+    }
+    channel.basic_publish(exchange='',
                       routing_key='com.hotellistat.scraping',
                       body=json.dumps(message))
+
 
 connection.close()
