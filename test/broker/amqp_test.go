89e432ff198d9fd3fb67c2c0d4e75b849466d0a4
Daniel Schneider <daniel.schneider@eramux.com>
2021-05-19 09:14:35 +0200

    test: improved broker testing

--- test/broker/amqp_test.go
+++ test/broker/amqp_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
 )
 
 // func TestAmqpConnect(t *testing.T) {
@@ -37,6 +38,7 @@ import (
 // }
 
 func TestAmqpInitialize(t *testing.T) {
+
 	godotenv.Load()
 
 	broker := broker.AMQPBroker{}
@@ -56,6 +58,43 @@ func TestAmqpInitialize(t *testing.T) {
 	jobManifest := job.NewManifest(2)
 
 	broker.Initialize(config, &jobManifest)
+
+	conn, err := amqp.Dial(os.Getenv("TEST_AMQP_BROKER_DSN"))
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	testChannel, testErr := conn.Channel()
+
+	if testErr != nil {
+		t.Fatal(testErr.Error())
+	}
+
+	_, queueDeclaredErr := testChannel.QueueInspect("test-subject")
+
+	if queueDeclaredErr != nil {
+		t.Fatal("Broker did not declare queue")
+	}
+
+	queue, queueErr := testChannel.QueueDeclare(
+		"test-subject",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if queueErr != nil {
+		t.Fatal(queueErr.Error())
+	}
+
+	t.Log(queue.Consumers)
+
+	if queue.Consumers < 1 {
+		t.Error("Broker did not initialize consumer")
+	}
 
 	if !broker.IsRunning() {
 		t.Error("Broker should be running after Initialization")
