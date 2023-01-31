6faedd0fdaedd065b09df9e70aaefc5e9175ecd7
Arnulf Wiedemann <arnulf.wiedemann@hotellistat.de>
2023-01-31 10:41:54 +0100

    fix: accept StatusCode 202 as ok

--- pkg/cloudevent/cloudevent.go
+++ pkg/cloudevent/cloudevent.go
@@ -22,16 +22,16 @@ func PublishEvent(event cloudevents.Event, masterResponsePort string) error {
 
 	masterPublishUrl := fmt.Sprintf(PublishUrlTemplate, masterResponsePort)
 	if eventBytes, err = json.Marshal(event); err != nil {
-		log.Printf("ERROR in Marshall(event): %v\n", err)
+		log.Printf("ERROR in PublishEvent Marshall(event): %v\n", err)
 		return err
 	}
-	log.Printf("SendEvent for eventID: %s\n", event.ID())
+	log.Printf("PublishEvent for eventID: %s\n", event.ID())
 	if response, err = http.Post(masterPublishUrl, "text/plain", bytes.NewBuffer(eventBytes)); err != nil {
 		log.Printf("ERROR in publish request: %v\n", err)
 		return err
 	}
-	if response != nil && response.StatusCode != http.StatusOK {
-		err = errors.New(fmt.Sprintf("ERROR response.StatusCode: %v err: %v\n", response.StatusCode, err))
+	if response != nil && response.StatusCode != http.StatusOK && response.StatusCode != http.StatusAccepted {
+		err = errors.New(fmt.Sprintf("ERROR PublishEvent response.StatusCode: %v err: %v\n", response.StatusCode, err))
 		log.Printf("%v\n", err)
 		return err
 	}
@@ -49,12 +49,12 @@ func SendEvent(port string, source string, eventType string, eventID string, eve
 	event.SetID(eventID)
 	event.SetType(eventType)
 	if eventDataBytes, err = json.Marshal(eventDataValues); err != nil {
-		log.Printf("ERROR in Marshall(eventDataValues): %v\n", err)
+		log.Printf("ERROR in SendEvent Marshall(eventDataValues): %v\n", err)
 		return err
 	}
 	event.SetData(cloudevents.ApplicationJSON, eventDataBytes)
 	if PublishEvent(event, port); err != nil {
-		log.Printf("ERROR in SendEvent: %v\n", err)
+		log.Printf("ERROR in SendEvent PublishEvent: %v\n", err)
 		return err
 	}
 	return err
